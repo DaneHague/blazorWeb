59d6db767d26ca1927221d2f3808ff392bb32c30
Dane <danehague1@googlemail.com>
2023-01-23 17:31:01 +0000

    Added unit tests with bUnit

---
+++ TestProject/Counter.cs
@@ -0,0 +1,18 @@
+using BlazorApp2.Pages;
+using Bunit;
+
+namespace TestProject
+{
+    public class CounterTest
+    {
+        [Fact]
+        public void CounterIncr()
+        {
+            using var ctx = new TestContext();
+            var cut = ctx.RenderComponent<Counter>();
+            cut.Find("button").Click();
+
+            cut.Find("p").MarkupMatches("<p role=\"status\">Current count: 1</p>");
+        }
+    }
+}
