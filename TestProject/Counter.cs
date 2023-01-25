009eb197718ff3ee08299b87cc7f39e93c20246d
Dane <danehague1@googlemail.com>
2023-01-25 12:57:46 +0000

    Updated Test - TODO : Inject service in bUnit

--- TestProject/Counter.cs
+++ TestProject/Counter.cs
@@ -1,5 +1,11 @@
+using AngleSharp;
+using BlazorApp2.Data;
 using BlazorApp2.Pages;
 using Bunit;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace TestProject
 {
@@ -9,6 +15,7 @@ namespace TestProject
         public void CounterIncr()
         {
             using var ctx = new TestContext();
+
             var cut = ctx.RenderComponent<Counter>();
             cut.Find("button").Click();
 
